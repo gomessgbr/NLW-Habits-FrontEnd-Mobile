30ebc26355a4790d8b0d5ec71485c5c400d5c548
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-18 18:40:19 -0300

    refactor: delete useless components on app root

--- App.tsx
+++ App.tsx
@@ -1,36 +1,33 @@
-
-
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter'
-import { Loading } from './src/components/Loading';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/home";
 
 export default function App() {
-  const [fontsLoaded] =useFonts({
-    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
-  })
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
 
-  if(!fontsLoaded){
-    return(
-      <Loading/>
-    )
+  if (!fontsLoaded) {
+    return <Loading />;
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.Text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  Text:{
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
