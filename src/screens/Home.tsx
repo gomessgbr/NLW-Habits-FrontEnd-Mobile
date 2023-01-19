f9b56f179ef92d4f8cece0c27b3e419489cdce63
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-19 13:38:41 -0300

    feat: add navigation to habit screen on click button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { generateDatesFromYearBeginning } from "../utils/genarate-dates-from-yea
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateDatesFromYearBeginning();
@@ -11,6 +12,8 @@ const minimumSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -25,15 +28,21 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:100}}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, i) => (
               <View
-              key={i}
+                key={i}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40 "
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
